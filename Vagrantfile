4c820d0c1780e9a3b077aff67f461def4f2b19a1
Stacy Danilova <sdanilova@express42.com>
2016-03-11 12:16:45 +0300

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,14 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "parallels/ubuntu-14.04"
+  config.vm.provider :parallels do |parallels, conf|
+    conf.vm.provision :chef_zero do |chef|
+      chef.log_level = :info
+      chef.cookbooks_path = "cookbooks"
+      chef.roles_path = "roles"
+      #chef.add_recipe "app_demo"
+      chef.data_bags_path = "data_bags"
+      chef.add_role "application"
+      chef.nodes_path = "nodes"
+    end
+  end
+end
---
+++ cookbooks/app_demo/recipes/default.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: app_demo
+# Recipe:: default
+#
+# Copyright 2016, Express42
+#
+# All rights reserved - Do Not Redistribute
+#
+package "nginx" do
+  action :install
+end
+
+service "nginx" do
+  action [:enable, :start]
+end
+
+template "/usr/share/nginx/html/index.html" do
+  source "index.html.erb"
+  variables(
+      food: node['app-demo']['food']
+    )
+end
+
+template "/usr/share/nginx/html/countries.html" do
+  source "countries.html.erb"
+  variables(
+    countries: data_bag('countries')
+  )
+end
+
+data_bag('countries').each do |country|
+  template "/usr/share/nginx/html/#{country}.html" do
+    source "country.html.erb"
+    variables(
+      country: country,
+      cities: data_bag_item('countries', country)['cities']
+    )
+  end
+end
