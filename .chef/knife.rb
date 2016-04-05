9e945f71ddd12ebc57dd57b64515b2c7bb36ca11
Stacy Danilova <sdanilova@express42.com>
2016-04-05 18:55:24 +0300

    chef server

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,1 +1,11 @@
-knife["data_bag_path"]="/Users/Stacy/Documents/devops/data_bags"
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "chloeraven"
+client_key               "#{current_dir}/chloeraven.pem"
+validation_client_name   "domk-validator"
+validation_key           "#{current_dir}/domk-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/domk"
+cookbook_path            ["#{current_dir}/../cookbooks"]
